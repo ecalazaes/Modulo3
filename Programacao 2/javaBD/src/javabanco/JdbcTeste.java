8655cf0eb80b6a1392b3852d720546237a1aa1ae
ADS 2023.1N <36129382023.1N@senacrjedu.local>
2024-06-06 21:35:48 -0300

    Modulo 3

---
+++ Programacao 2/javaBD/src/javabanco/JdbcTeste.java
@@ -0,0 +1,9 @@
+package javabanco;
+
+import java.sql.SQLException;
+
+public class JdbcTeste {
+    public static void main(String[] args) throws SQLException {
+//        System.out.println("Conectado ao Banco de Dados local!");
+    }
+}
---
+++ Programacao 2/javaBD/src/javabanco/TestaFabricaDeConexoes.java
@@ -0,0 +1,22 @@
+package javabanco;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class TestaFabricaDeConexoes {
+    static Connection con;
+    public static void main(String[] args) {
+        try {
+            con = ConnectionFactory.getConnection();
+            System.out.println("Conectado ao Banco Local!");
+        } catch (SQLException e ) {
+            e.printStackTrace();
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
