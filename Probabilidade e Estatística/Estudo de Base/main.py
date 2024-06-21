56e483165cfd81a874a1fe80adacd7354bf72ee2
Erick Calazães <ecalazaes@gmail.com>
2024-06-21 00:33:23 -0300

    Modulo 3

--- Probabilidade e Estatística/Estudo de Base/main.py
+++ Probabilidade e Estatística/Estudo de Base/main.py
@@ -10,5 +10,21 @@ df = df.dropna()
 # Substituir zeros por NaN e depois remover linhas com NaN
 df = df.replace(0, pd.NA).dropna()
 
+# Formatando as colunas desejadas
+df['Código da Receita Tributária'] = df['Código da Receita Tributária'].map('{:.2f}'.format)
+df['Valor da Receita Tributária'] = df['Valor da Receita Tributária'].map('R$ {:,.2f}'.format)
+df['Percentual do PIB'] = df['Percentual do PIB'].map('{:.2f}%'.format)
+
+# Ordenar por Ano-calendário e Código da Receita Tributária
+df = df.sort_values(by=['Ano-calendário', 'Código da Receita Tributária'])
+
 # Exibir a tabela formatada
+print("### Tabela Completa ###")
 print(tabulate(df, headers='keys', tablefmt='pretty'))
+
+# Encontrar os maiores valores de Valor da Receita Tributária por Ano-calendário
+# max_values = df.loc[df.groupby('Ano-calendário')['Valor da Receita Tributária'].idxmax()]
+
+# Exibir os maiores valores de Valor da Receita Tributária por Ano-calendário
+# print("\n### Maiores Valores por Ano-calendário ###")
+# print(tabulate(max_values, headers='keys', tablefmt='pretty'))
